1cbfc1f86770cbbd78d3d7e1051194dcc5cb7d5e
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-09-10 14:49:14 -0300

    toBe exemplos
    O matcher toBe, a princípio, parece ser igual ao toEqual. A diferença é que toBe verifica não só se os dois valores são iguais, mas também se eles são do mesmo objeto.

---
+++ jasmine/spec/testesSpec.js
@@ -0,0 +1,8 @@
+describe("Testes (specs)", function(){
+	it("Deve garantir que o valor 'true'  é 'true'", function(){
+
+	});
+	it("Deve garantir que o valor 'true' não é 'false'", function(){
+		
+	});
+});
