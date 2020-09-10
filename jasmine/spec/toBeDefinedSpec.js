ab2245ef765840adbcb3143bb472ae356a9845c7
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-09-10 17:56:16 -0300

    Operador toBeDefined
    verificam se um valor é defined ou undefined.

---
+++ jasmine/spec/toBeDefinedSpec.js
@@ -0,0 +1,8 @@
+describe("Operador toBeDefined",function(){
+	it("Deve validar o operador toBeDefined",function(){
+		var texto = "Samuel";
+		var texto2;
+		expect(texto).toBeDefined();
+		expect(texto2).not.toBeDefined();
+	});
+});
